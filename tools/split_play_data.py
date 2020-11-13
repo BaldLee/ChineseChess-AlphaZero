7a6fcb85ab2b6eed5790eeec0aa8f9bd8187e989
Jiahang Xie <1351533120@sjtu.edu.cn>
2020-11-13 18:27:31 +0800

    add tool

--- tools/split_play_data.py
+++ tools/split_play_data.py
@@ -5,7 +5,10 @@ path = os.getcwd() + "/play_data"
 target = os.getcwd() + "/target"
 files = os.listdir(path)
 
-# for item in files:
+folder = os.path.exists(target)
+if not folder:          
+    os.makedirs(path)
+    print(f"create folder \"target/\"")
 
 for item in files:
     f = open(path + '/' + item, "rt")
@@ -17,7 +20,12 @@ for item in files:
             data = []
             datas.append(data)
         data.append(i)
-    for idx, d in enumerate(datas):
-        newname = target+'/'+item.split('.')[0]+'-'+str(idx)+".json"
-        with open(newname, "w") as fout:
-            json.dump(d, fout)
+    if len(datas) > 1:
+        print(f"file: {item} is wrong")
+        for idx, d in enumerate(datas):
+            newname = target+'/'+item.split('.json')[0]+'-'+str(idx)+".json"
+            with open(newname, "w") as fout:
+                json.dump(d, fout)
+    else:
+        with open(target+'/'+item, "w") as fout:
+            json.dump(datas[0], fout)
