e4ca299dfcd36973d3c82e241c13a20cb89ec1a1
Jiahang Xie <1351533120@sjtu.edu.cn>
2020-11-18 19:52:55 +0800

    update initenv

--- initenv.sh
+++ initenv.sh
@@ -1,6 +1,4 @@
 source activate
-git clone https://github.com.cnpmjs.org/BaldLee/ChineseChess-AlphaZero.git
 conda create -n chess --clone tensorflow_py3
 conda activate chess
-cd ChineseChess-AlphaZero
 pip install -r requirements.txt
