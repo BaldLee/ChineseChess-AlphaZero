291e4ab5d683d70fdbef3c225010e0905ddd70fd
Jiahang Xie <1351533120@sjtu.edu.cn>
2020-11-20 20:07:29 +0800

    test script

--- tools/predict_test.py
+++ tools/predict_test.py
@@ -1,23 +1,66 @@
+from multiprocessing import Process
 import os
 import json
-from Keras import Model
+import hashlib
+import numpy as np
+from keras.engine.training import Model
 import tensorflow as tf
+import keras.backend as K
 
-from logging import getLogger
-logger = getLogger(__name__)
-
-def load(self, config_path, weight_path):
+class tmp:
+    def __init__(self):
+        self.model = None
+        self.graph = None
+    def load(self, config_path, weight_path):
         if os.path.exists(config_path) and os.path.exists(weight_path):
-            logger.debug(f"loading model from {config_path}")
+            print(f"loading model from {config_path}")
             with open(config_path, "rt") as f:
                 self.model = Model.from_config(json.load(f))
             self.model.load_weights(weight_path)
-            logger.debug(f"loaded model digest = {self.digest}")
+            self.graph = tf.get_default_graph()
+            print(f"loaded model digest = {self.fetch_digest(weight_path)}")
             return True
         else:
-            logger.debug(f"model files does not exist at {config_path} and {weight_path}")
+            print(f"model files does not exist at {config_path} and {weight_path}")
             return False
+    @staticmethod
+    def fetch_digest(weight_path):
+        if os.path.exists(weight_path):
+            m = hashlib.sha256()
+            with open(weight_path, "rb") as f:
+                m.update(f.read())
+            return m.hexdigest()
+        return None
 
-s = None
+def get_path():
+    d = os.path.dirname
+    project_dir = d(d(os.path.abspath(__file__)))
+    data_dir = os.path.join(project_dir, "data")
+    model_dir = os.path.join(data_dir, "model")
+    config_path = os.path.join(model_dir, "model_best_config.json")
+    weight_path = os.path.join(model_dir, "model_best_weight.h5")
+    return config_path, weight_path
 
-load(s,"../data/model/model_best_config.json","../data/model/model_best_weight.h5")
+def worker():
+    s = tmp()
+    config_path,weight_path = get_path()
+    s.load(config_path,weight_path)
+    data = np.zeros(shape=(1, 14, 10, 9), dtype=np.float32)
+    with s.graph.as_default():
+        p,v = s.model.predict_on_batch(data)
+        print(f"p:{p}")
+        print(f"v:{v}")
+    print("done")
+
+tf.disable_v2_behavior()
+config = tf.ConfigProto(allow_soft_placement=True,gpu_options=tf.GPUOptions(visible_device_list='0'))
+sess = tf.Session(config=config)
+K.set_session(sess)
+# p1 = Process(target=worker)
+# p2 = Process(target=worker)
+# p1.start()
+# p2.start()
+# p1.join()
+# p2.join()
+# print("done")
+worker()
